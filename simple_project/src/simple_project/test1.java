2778fd5bcc4c3a139777862068fa1a8366957463
wu <wyt246890@gmail.com>
2023-07-30 09:20:28 +0800

    initial commit

---
+++ simple_project/src/simple_project/test1.java
@@ -0,0 +1,11 @@
+package simple_project;
+import java.util.*;
+public class test1 {
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("please enter an interger and a double");
+		int a=sc.nextInt();
+		double b=sc.nextDouble();
+	}
+
+}
